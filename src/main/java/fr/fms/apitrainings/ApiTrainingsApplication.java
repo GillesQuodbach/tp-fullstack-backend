7baf41cf5f5a30faba4c4ccc5723364b03745137
Gilles <quodbach.gilles@gmail.com>
2024-05-27 10:13:05 +0200

    ajout user.home

--- src/main/java/fr/fms/apitrainings/ApiTrainingsApplication.java
+++ src/main/java/fr/fms/apitrainings/ApiTrainingsApplication.java
@@ -29,6 +29,7 @@ public class ApiTrainingsApplication implements CommandLineRunner {
 	}
 
 	public void generatedData() {
+		
 		List<Training> trainingsList = new ArrayList<>();
 
 		Category informatique = new Category("Informatique", trainingsList);
@@ -41,29 +42,29 @@ public class ApiTrainingsApplication implements CommandLineRunner {
 		categoryRepository.save(anglais);
 		categoryRepository.save(finance);
 		
-		trainingRepository.save(new Training(null, "Java", "Formation Java", 150, 1, "shoes.jpg",informatique));
-		trainingRepository.save(new Training(null, "C", "Formation C", 100, 1, "lion.jpg", informatique));
-		trainingRepository.save(new Training(null, "Javascript", "Formation Javascript", 120, 1, "lake.jpg", informatique));
-		trainingRepository.save(new Training(null, "Python", "Formation Python", 300, 1, "lake.jpg", informatique));
-		trainingRepository.save(new Training(null, "NodeJS", "Formation NodeJS", 175, 1, "lake.jpg", informatique));
+		trainingRepository.save(new Training(null, "Java", "Formation Java", 150, 1, "default.jpg",informatique));
+		trainingRepository.save(new Training(null, "C", "Formation C", 100, 1, "default.jpg", informatique));
+		trainingRepository.save(new Training(null, "Javascript", "Formation Javascript", 120, 1, "default.jpg", informatique));
+		trainingRepository.save(new Training(null, "Python", "Formation Python", 300, 1, "default.jpg", informatique));
+		trainingRepository.save(new Training(null, "NodeJS", "Formation NodeJS", 175, 1, "default.jpg", informatique));
 
-		trainingRepository.save(new Training(null, "Bases", "Les bases de la cuisine", 90, 1, "lake.jpg", cuisine));
-		trainingRepository.save(new Training(null, "Cuisine du monde", "Les cuisines du monde", 300, 1, "lake.jpg", cuisine));
-		trainingRepository.save(new Training(null, "Pâtisserie", "Les bases de la pâtisserie", 310, 1, "lake.jpg", cuisine));
-		trainingRepository.save(new Training(null, "Vegan", "Apprendre la cuisine vegan", 45, 1, "lake.jpg", cuisine));
-		trainingRepository.save(new Training(null, "Santé", " Élaboration de plats équilibrés", 155, 1, "lake.jpg", cuisine));
+		trainingRepository.save(new Training(null, "Bases", "Les bases de la cuisine", 90, 1, "default.jpg", cuisine));
+		trainingRepository.save(new Training(null, "Cuisine du monde", "Les cuisines du monde", 300, 1, "default.jpg", cuisine));
+		trainingRepository.save(new Training(null, "Pâtisserie", "Les bases de la pâtisserie", 310, 1, "default.jpg", cuisine));
+		trainingRepository.save(new Training(null, "Vegan", "Apprendre la cuisine vegan", 45, 1, "default.jpg", cuisine));
+		trainingRepository.save(new Training(null, "Santé", " Élaboration de plats équilibrés", 155, 1, "default.jpg", cuisine));
 
-		trainingRepository.save(new Training(null, "Anglais", "Formation à l'anglais", 150, 1, "lake.jpg", anglais));
-		trainingRepository.save(new Training(null, "Espagnol", "Formation à l'espagnol", 100, 1, "lake.jpg", anglais));
-		trainingRepository.save(new Training(null, "Français", "Formation au français", 120, 1, "lake.jpg", anglais));
-		trainingRepository.save(new Training(null, "Allemand", "Formation à l'allemand", 300, 1, "lake.jpg", anglais));
-		trainingRepository.save(new Training(null, "Mandarin", "Formation au mandarin", 175, 1, "lake.jpg", anglais));
+		trainingRepository.save(new Training(null, "Anglais", "Formation à l'anglais", 150, 1, "default.jpg", anglais));
+		trainingRepository.save(new Training(null, "Espagnol", "Formation à l'espagnol", 100, 1, "default.jpg", anglais));
+		trainingRepository.save(new Training(null, "Français", "Formation au français", 120, 1, "default.jpg", anglais));
+		trainingRepository.save(new Training(null, "Allemand", "Formation à l'allemand", 300, 1, "default.jpg", anglais));
+		trainingRepository.save(new Training(null, "Mandarin", "Formation au mandarin", 175, 1, "default.jpg", anglais));
 
-		trainingRepository.save(new Training(null, "Comptabilité", "Les bases de la comptabilité", 150, 1, "lake.jpg", finance));
-		trainingRepository.save(new Training(null, "Analyse", "Formation aux analyses financières", 100, 1, "lake.jpg", finance));
-		trainingRepository.save(new Training(null, "Gestion", "Formation au gestion budgétaire", 120, 1, "lake.jpg", finance));
-		trainingRepository.save(new Training(null, "Fiscalité", "Fiscalité des entreprises", 300, 1, "lake.jpg", finance));
-		trainingRepository.save(new Training(null, "Investissement", "Investissement et gestion de portefeuille", 175, 1, "lake.jpg", finance));
+		trainingRepository.save(new Training(null, "Comptabilité", "Les bases de la comptabilité", 150, 1, "default.jpg", finance));
+		trainingRepository.save(new Training(null, "Analyse", "Formation aux analyses financières", 100, 1, "default.jpg", finance));
+		trainingRepository.save(new Training(null, "Gestion", "Formation au gestion budgétaire", 120, 1, "default.jpg", finance));
+		trainingRepository.save(new Training(null, "Fiscalité", "Fiscalité des entreprises", 300, 1, "default.jpg", finance));
+		trainingRepository.save(new Training(null, "Investissement", "Investissement et gestion de portefeuille", 175, 1, "default.jpg", finance));
 
 	}
 
